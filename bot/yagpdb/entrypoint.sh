aad63d8c4ffab7369e493650432580f356c7a0ee
darksaid98 <darksaid9889@gmail.com>
2024-12-31 05:08:25 +0100

    fix(yagpdb): add binary

--- bot/yagpdb/entrypoint.sh
+++ bot/yagpdb/entrypoint.sh
@@ -26,12 +26,11 @@
 TZ=${TZ:-UTC}
 export TZ
 
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 # Switch to the container's working directory
 cd /home/container || exit 1
+
+# Add custom binary
+export PATH=$PATH:/root/.local/bin
 
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
